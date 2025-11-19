156aeacb8a61c92afea9fdbcd6bad503e19ac3df
rbhullar2000 <robsbhullar@gmail.com>
2025-11-19 15:27:57 -0800

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,8 @@
 import { type NextRequest, NextResponse } from "next/server"
 import nodemailer from "nodemailer"
 
+export const runtime = "nodejs"
+
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json()
